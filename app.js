4f2a61815c5aad8879f1354ea9fa499b7377ae3b
Karie <karie.jpg@hotmail.com>
2025-09-23 17:14:57 -0500

    actualización de app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 
 const inputAmigo = document.getElementById("amigo");
-const listaAmigos= []; 
+const listaAmigos= []; //Array de la lista de amigos
 const ulListaAmigos= document.getElementById("listaAmigos");
 const ulResultado = document.getElementById ("resultado"); 
 
+//función para agregar un amigo
+//campo de texto
+//Botón
 function agregarAmigo () {
     listaAmigos.push(inputAmigo.value);
     ulListaAmigos.innerHTML += `<li>${inputAmigo.value} </li>`;
+
+    //validar que el campo no esté vacío
+    //// validar que no esté repetido el nombre
 }
 
-function sortearAmigo () {
+//botón sortear amigo
+function sortearAmigo() {
     const aleatorio = Math.floor (Math.random()* listaAmigos.length);
     const amigoSecreto= listaAmigos [aleatorio];
     ulResultado.innerHTML= `<li> Tu amigo amigo secreto es: ${amigoSecreto} </li>`;
